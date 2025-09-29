6280edf3d4767311c56694777227f6f6ea884a2d
UB2002 <udaybhaskar0532@gmail.com>
2025-09-29 16:12:13 +0530

    live frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000", // backend URL
+  baseURL: "https://data-agent-27lk.onrender.com", // backend URL
 });
 
 export const uploadFile = async (file) => {
